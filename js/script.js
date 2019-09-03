3d9bf1cfae0fc297475c0c7e319f5826fbf2b71e
ar2unf <dim960@gmail.com>
2019-09-03 20:22:37 +0300

    rebild on class

--- js/script.js
+++ js/script.js
@@ -1,21 +1,61 @@
-const goods = [
+/*const goods = [
     {title: 'Shirt', price: 150},
     {title: 'Socks', price: 50},
     {title: 'Jacket', price: 350},
     {title: 'Shoes', price: 250},
-];
+];*/
 
-const renderGoodsItem = (title ='goods', price =0) =>  // уберем return укажем значения по умочанию
+/*const renderGoodsItem = (title ='goods', price =0) =>  // уберем return укажем значения по умочанию
     `<div class="goods-item">
       <h3>${title}</h3>
       <p>${price}</p>
-    </div>`;
+    </div>`;*/
 
-const renderGoodsList = list => { //уберем скобки
+/*const renderGoodsList = list => { //уберем скобки
     const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
 
-        /*document.querySelector('.goods-list').innerHTML = goodsList;*/ // выводит массив с запятыми
+        
     document.querySelector('.goods-list').innerHTML = goodsList.join(''); // преобразуем массив в строку без разделителей
-}
+}*/
 
-window.onload = () => renderGoodsList(goods); // уберем фигурные скобки
+/*window.onload = () => renderGoodsList(goods); // уберем фигурные скобки*/
+class GoodsItem {
+    constructor(title = 'No name', price = 'No price') {
+      this.title = title;
+      this.price = price;
+    }
+    render() {
+      return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+  }
+  
+  class GoodsList {
+    constructor(container = '.container') {
+      this.container = container;
+      this.goods = [];
+    }
+    fetchGoods() {
+      this.goods = [
+        {title: 'Shirt', price: 150},
+        {title: 'Socks', price: 50},
+        {title: 'Jacket', price: 350},
+        {title: 'Shoes', price: 250},
+        {price: 350},
+        {title: 'lambutens'}
+      ];
+    }
+    render() {
+      document.querySelector(this.container).innerHTML = this.goods.reduce((acc, item) => {
+        const good = new GoodsItem(item.title, item.price);
+        return acc += good.render();
+      }, '');
+    }
+  }
+  
+  
+  document.addEventListener('DOMContentLoaded', ()=>{
+    const list = new GoodsList('.goods-list');
+    list.fetchGoods();
+    list.render();
+  }, false)
+  
