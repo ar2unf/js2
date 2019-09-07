be72633a80b1a2c2973e4eb27cb78fb48c0cb8e0
ar2unf <dim960@gmail.com>
2019-09-07 09:34:51 +0300

    исправления по уроку.

--- js/script.js
+++ js/script.js
@@ -1,107 +1,76 @@
-/*const goods = [
-    {title: 'Shirt', price: 150},
-    {title: 'Socks', price: 50},
-    {title: 'Jacket', price: 350},
-    {title: 'Shoes', price: 250},
-];*/
+const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-/*const renderGoodsItem = (title ='goods', price =0) =>  // уберем return укажем значения по умочанию
-    `<div class="goods-item">
-      <h3>${title}</h3>
-      <p>${price}</p>
-    </div>`;*/
+function makeGETRequest(url, callback) {
+  const xhr = window.XMLHttpRequest ? new window.XMLHttpRequest() : new window.ActiveXObject('Microsoft.XMLHTTP');
 
-/*const renderGoodsList = list => { //уберем скобки
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-
-        
-    document.querySelector('.goods-list').innerHTML = goodsList.join(''); // преобразуем массив в строку без разделителей
-}*/
-
-/*window.onload = () => renderGoodsList(goods); // уберем фигурные скобки*/
-class GoodsItem {
-    constructor(title = 'No name', price = 'No price') {
-      this.title = title;
-      this.price = price;
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText)
     }
-    /** рендер товара */
-    render() {
-      return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-    }
-  }
-  
-  class GoodsList {
-    constructor(container = '.container') {
-      this.container = container;
-      this.goods = [];
-    }
-    /**заполняем массив */
-    fetchGoods() {
-      this.goods = [
-        {title: 'Shirt', price: 150},
-        {title: 'Socks', price: 50},
-        {title: 'Jacket', price: 350},
-        {title: 'Shoes', price: 250},
-        {price: 350},
-        {title: 'lambutens'}
-      ];
-    }
-    /**
-     * рендер списка товаров
-     */
-    render() {
-      document.querySelector(this.container).innerHTML = this.goods.reduce((acc, item) => {
-        const good = new GoodsItem(item.title, item.price);
-        return acc += good.render();
-      }, '');
-    }
-    /**
-     * метод определяющий суммарную соимость товаров списка
-     */
-    getTotalPriceGoods(){
-        return this.goods.reduce((acc,item)=>{
-            /**проверям на число */
-            let isNumber =item.price!='No price' && item.price!=undefined
-            return acc += isNumber ? Number(item.price) : 0;
-        }, 0)
-    }
-  }
-  /**расширения для товаров в корзине */
-  class GoodsBasketItem extends GoodsItem {
-      constructor(title, price, count=1){
-          super(title,price);
-          this.count=count;
-      }
-      
-  }
-/**класс корзины */
-  class Basket{
-      constructor(goods =[]){
-          this.goods =goods;
-      }
-      /**инициализация передача других объектов, создание в DOM */
-      init(){}
-      /** добавить в корзину товар*/
-      addGood(Goodsitem, count =1){}
-      /**удалить из корзины товар */
-      delGood(GoodsItem, count =1){}
-      /**изменить количество товара */
-      setCountGood(){}
-      /**очистить корзину */
-      clearBasket(){}
-      /**посчитать стоимость */
-      totalPriceGoods(){}
-      /** обновления в DOM */
-      render(){}
-      /**проверка на пустоту для активации заказа */
-      isOrder(){}
   };
-  
-  
-  document.addEventListener('DOMContentLoaded', ()=>{
-    const list = new GoodsList('.goods-list');
-    list.fetchGoods();
-    list.render();
-    console.log(list.getTotalPriceGoods()); //тест функции getTotalPriceGoods
-  }, false)
-  
+
+  xhr.open('GET', url);
+  xhr.send();
+}
+
+
+
+
+class GoodsItem {
+  constructor(title = 'No name', price = 'No price') {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  }
+}
+
+class GoodsList {
+  constructor(container = '.container') {
+    this.container = container;
+    this.goods = [];
+  }
+  totalPrice() {
+    return this.goods.reduce((total, good) => {
+      if (!good.price) return total;
+      return total += good.price;
+    }, 0);
+  }
+  fetchGoods(cb) {
+    makeGETRequest(`${BASE_URL}/catalogData.json`, (goods) => {
+      this.goods = JSON.parse(goods);
+      cb();
+    });
+  }
+  render() {
+    document.querySelector(this.container).innerHTML = this.goods.reduce((acc, item) => {
+      const good = new GoodsItem(item.product_name, item.price);
+      return acc += good.render();
+    }, '');
+  }
+}
+
+class Cart extends GoodsList {
+  add(good) {}
+  remove(id) {
+    if (!id) {
+      // clean cart
+      return;
+    }
+  }
+  update(id, good) {}
+}
+
+class CartItem extends GoodsItem {
+  constructor(title = 'No name', price = 'No price', count = 1) {
+    super(title, price);
+    this.count = count;
+  }
+}
+
+
+const list = new GoodsList('.goods-list');
+list.fetchGoods(() => {
+  list.render();
+});
